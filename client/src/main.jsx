f2ea8a00a2c5eb8a7630fb773870daaa74462825
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-16 21:22:53 -0300

    Main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './style.css'
 
-
-const root =  createRoot(document.getElementById('app'))
+const root = createRoot(document.getElementById('app'))
 
 root.render(
-    <App />
+  <App />
 )
