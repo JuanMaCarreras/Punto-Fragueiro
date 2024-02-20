42ffcdc319e3f341af5c39a5066c09e71fb7b539
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-19 22:30:04 -0300

    Nav Bar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+function NavBar () {
+  return (
+    <>
+      <div>
+        <img src='https://www.pizzascordobapuntofragueiro.com.ar/imagenes/logo3sinfondo.png' alt='' />
+      </div>
+      <div>
+        <ul>
+          <li>Inicio</li>
+          <li>Menu</li>
+          <li>Ubicación</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default NavBar
