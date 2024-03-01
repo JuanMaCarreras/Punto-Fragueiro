4e7853b1b2912b5dc52da2cbd97faa9276944b54
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-29 23:04:28 -0300

    Home style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home () {
 
       <main className='bg-blue-800 flex flex-col justify-center pl-11 h-96 '>
         <h1 className='text-5xl'>Pizzería Punto Fragueiro</h1>
-        <p>Un lugar con buena onda</p>
+        <p className='text-white text-xl'>Un lugar con buena onda</p>
       </main>
 
     </>
