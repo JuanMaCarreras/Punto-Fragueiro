6ec322987cf43140829114c21a16b38d3ed5f81c
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-04 13:08:39 -0300

    background Img

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home () {
     <>
       <NavBar />
 
-      <main className='bg-blue-800 flex flex-col justify-center pl-11 h-96 '>
+      <main className='flex flex-col justify-center pl-11 bg-cover bg-center h-screen bg-mainImg'>
         <h1 className='text-5xl'>Pizzería Punto Fragueiro</h1>
         <p className='text-white text-xl'>Un lugar con buena onda</p>
       </main>
