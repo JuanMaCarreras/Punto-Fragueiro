655c7cac408c781316b91af0c58a21c86462c7b0
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-04 13:08:25 -0300

    extend background Img

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,jsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        mainImg: 'url(assets/pizza-humo.jpg)'
+      }
+    }
   },
   plugins: []
 }
