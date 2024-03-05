e9e63da17d6041bf1679dc237c66ac695418b5e6
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-05 20:45:55 -0300

    Home Style - center title

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,8 +6,8 @@ function Home () {
       <NavBar />
 
       <main className='flex flex-col justify-center pl-11 bg-cover bg-center h-screen bg-mainImg'>
-        <h1 className='text-5xl'>Pizzería Punto Fragueiro</h1>
-        <p className='text-white text-xl'>Un lugar con buena onda</p>
+        <h1 className='text-5xl mt-[-100px] text-white'>Pizzería Punto Fragueiro</h1>
+        <p className='text-white text-xl '>Un lugar con buena onda</p>
       </main>
 
     </>
