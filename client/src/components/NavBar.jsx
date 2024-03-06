882c3dbf5e3fbc3d110334e142127a28edbedfc1
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-05 21:09:45 -0300

    NavBar Style

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,14 +2,14 @@ function NavBar () {
   return (
     <>
       <header className='flex justify-between items-center bg-zinc-900 top-0 w-full'>
-        <div>
+        <div className='pl-10'>
           <img
             className='h-24 w-24'
             src='https://www.pizzascordobapuntofragueiro.com.ar/imagenes/logo3sinfondo.png'
             alt='logo'
           />
         </div>
-        <nav >
+        <nav className='pr-10'>
           <ul className='flex  text-white space-x-6 pl-10'>
             <li>Inicio</li>
             <li>Menu</li>
