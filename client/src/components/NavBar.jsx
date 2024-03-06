5f4f4fe0689967b4105347ae1c023556503ac945
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-06 13:04:21 -0300

    fixed NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,24 @@
 function NavBar () {
   return (
     <>
-      <header className='flex justify-between items-center bg-zinc-900 top-0 w-full'>
-        <div className='pl-10'>
-          <img
-            className='h-24 w-24'
-            src='https://www.pizzascordobapuntofragueiro.com.ar/imagenes/logo3sinfondo.png'
-            alt='logo'
-          />
-        </div>
-        <nav className='pr-10'>
-          <ul className='flex  text-white space-x-6 pl-10'>
-            <li>Inicio</li>
-            <li>Menu</li>
-            <li>Ubicación</li>
-          </ul>
-        </nav>
-      </header>
+      <nav className='relative'>
+        <header className='flex justify-center items-center bg-zinc-900 top-0 w-full'>
+          <div className=''>
+            <img
+              className='h-24 w-24'
+              src='https://www.pizzascordobapuntofragueiro.com.ar/imagenes/logo3sinfondo.png'
+              alt='logo'
+            />
+          </div>
+          <nav className=''>
+            <ul className='flex  text-white space-x-6 pl-10'>
+              <li>Inicio</li>
+              <li>Menu</li>
+              <li>Ubicación</li>
+            </ul>
+          </nav>
+        </header>
+      </nav>
     </>
   )
 }
