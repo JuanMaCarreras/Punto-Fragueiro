2deb22dc0d83823024cf14f9e695d0f4e4e033f4
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-06 13:02:38 -0300

    render NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 function App () {
   return (
-    <Home />
+    <>
+      <NavBar />
+      <Home />
+      <Home />
+      <Home />
+    </>
   )
 }
 
