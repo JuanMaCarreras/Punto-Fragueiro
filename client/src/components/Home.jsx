113455db5b0eb1abe63615a3c5766f7864fc72bc
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-06 13:03:05 -0300

    Home style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,6 @@
-import NavBar from './NavBar'
-
 function Home () {
   return (
     <>
-      <NavBar />
-
       <main className='flex flex-col justify-center pl-11 bg-cover bg-center h-screen bg-mainImg'>
         <h1 className='text-5xl mt-[-100px] text-white'>Pizzería Punto Fragueiro</h1>
         <p className='text-white text-xl '>Un lugar con buena onda</p>
