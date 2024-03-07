9ff5e8a5219dac41b36631bcd2c2ee18d812eedf
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-07 11:44:52 -0300

    menu structure

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+function Menu () {
+  return (
+    <>
+      <section className=''>
+        <article className='border rounded-sm border-red-600'>
+          <ul>
+            <li>Menu</li>
+          </ul>
+        </article>
+      </section>
+    </>
+  )
+}
+
+export default Menu
