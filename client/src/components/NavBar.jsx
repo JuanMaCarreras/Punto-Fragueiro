9c10e2f7f27edd919588af43b6d171ec34c7a047
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-08 13:10:52 -0300

    location and contact are added to the Nav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import Logo from '../assets/logo.png'
 function NavBar () {
   return (
     <>
-      <header className='flex items-center fixed top-0 w-full justify-between z-40 text-white bg-zinc-900'>
-        <div className='pr-11'>
+      <header className='flex items-center fixed justify-between px-11 top-0 w-full  text-white bg-zinc-900'>
+        <div className=''>
           <img
             className='h-20 w-20'
             src={Logo}
@@ -12,13 +12,21 @@ function NavBar () {
           />
         </div>
 
-        <nav className=''>
+        <nav className='mr-[-50px]'>
           <ul className='flex  text-white space-x-6 '>
             <li>Inicio</li>
             <li>Menú</li>
             <li>Ubicación</li>
           </ul>
         </nav>
+
+        <nav className=''>
+          <ul className='flex   text-white space-x-6 '>
+            <li>Horarios</li>
+            <li>WhatsApp</li>
+          </ul>
+        </nav>
+
       </header>
     </>
   )
