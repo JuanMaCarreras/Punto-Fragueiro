4b5122e1975115f6404d3e14f3f56d0b0dc52884
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-08 12:48:46 -0300

    NavBar style

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,19 +3,19 @@ import Logo from '../assets/logo.png'
 function NavBar () {
   return (
     <>
-      <header className='fixed w-screen'>
-        <nav className='flex justify-center items-center bg-zinc-900 top-0 '>
-          <div className='pr-11'>
-            <img
-              className='h-20 w-20'
-              src={Logo}
-              alt='logo'
-            />
-          </div>
+      <header className='flex items-center fixed top-0 w-full justify-between z-40 text-white bg-zinc-900'>
+        <div className='pr-11'>
+          <img
+            className='h-20 w-20'
+            src={Logo}
+            alt='logo'
+          />
+        </div>
 
+        <nav className=''>
           <ul className='flex  text-white space-x-6 '>
             <li>Inicio</li>
-            <li>Menu</li>
+            <li>Menú</li>
             <li>Ubicación</li>
           </ul>
         </nav>
