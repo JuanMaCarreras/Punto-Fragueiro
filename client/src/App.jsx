23856edd72b9fa5b657c441fd584fb709732bfa7
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-08 13:06:36 -0300

    Menu render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import Home from './components/Home'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Menu from './components/Menu'
 
 function App () {
   return (
     <>
       <NavBar />
       <Home />
-      <Home />
+      <Menu />
       <Home />
     </>
   )
