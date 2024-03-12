61dfa70c2550535285ab03dfaa4cbe9a4fa828b4
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-11 21:39:54 -0300

    Menu Structure

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,19 +1,21 @@
 function Menu () {
   return (
     <>
-      <section className=''>
-        <article className='bg-menuBackground bg-no-repeat h-screen '>
-          <h3>Menu</h3>
-          <h4>Pizzas</h4>
-          <div>
-            <ul>
-              <li>Pizza con Huevo</li>
-              <li>Menu</li>
-              <li>Menu</li>
-              <li>Menu</li>
-              <li>Menu</li>
-            </ul>
-          </div>
+      <section className='bg-gray-900 h-screen'>
+        <h2 className='text-5xl'>Menú</h2>
+        <h3 className='text-4xl'>Pizzas</h3>
+        <article className='bg-zinc-800 border border-green-600 mx-96 pb-80'>
+          <ul
+            className='p-11  text-white'
+          >
+            <li>
+              <h4>Muzzarella</h4>
+            </li>
+            <li>Menu</li>
+            <li>Menu</li>
+            <li>Menu</li>
+            <li>Menu</li>
+          </ul>
         </article>
       </section>
     </>
