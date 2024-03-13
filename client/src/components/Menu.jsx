fc545537a138c1d1fe67ab624cea6104477e326e
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-13 14:49:40 -0300

    background menu

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,7 +1,7 @@
 function Menu () {
   return (
     <>
-      <section className='bg-zinc-800 h-screen'>
+      <section className='bg-menuBackground h-screen'>
         <h2 className='text-5xl'>Menú</h2>
         <h3 className='text-4xl'>Pizzas</h3>
         <article className='bg-gray-900 border border-green-600 mx-96 pb-80'>
