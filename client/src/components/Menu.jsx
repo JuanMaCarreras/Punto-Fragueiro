a864516dd2d17950e13aeadfefe9b05cc9c9ea8e
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-15 13:00:00 -0300

    menu restrucure

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -2,22 +2,16 @@ function Menu () {
   return (
     <>
       <section className='bg-menuBackground  h-screen'>
-        <h2 className='flex text-4xl justify-center text-white'>Menú</h2>
-        <article className='bg-gray-900 border border-green-600 mx-96 pb-80'>
-          <h3 className='text-4xl'>Pizzas</h3>
-          <ul
-            className='p-6 text-white list-disc'
-          >
-            <li>
-              <h4>Muzzarella</h4>
-              <p cassName='font-'>Queso, Jamon, Morrones</p>
-            </li>
-            <li>Menu</li>
-            <li>Menu</li>
-            <li>Menu</li>
-            <li>Menu</li>
+        <aside className=''>
+          <ul className='text-white'>
+            <li>Pizzas</li>
+            <li>Hamburguesa</li>
+            <li>Lomitos</li>
+            <li>Empanadas</li>
+            <li>Sandwich</li>
           </ul>
-        </article>
+
+        </aside>
       </section>
     </>
   )
