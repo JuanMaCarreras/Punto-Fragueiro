4d85dbd94b5dbce9a58f4d33d294b49b18ab8787
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-18 21:38:25 -0300

    app

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,17 +1,23 @@
 function Menu () {
   return (
     <>
-      <section className='bg-menuBackground  h-screen'>
-        <aside className=''>
-          <ul className='text-white'>
-            <li>Pizzas</li>
-            <li>Hamburguesa</li>
-            <li>Lomitos</li>
-            <li>Empanadas</li>
-            <li>Sandwich</li>
-          </ul>
-
+      <section className='flex  bg-menuBackground h-screen py-28 px-11'>
+        <aside className='border border-red-500 mr-8'>
+          <nav className='pl-6 pr-11 py-8'>
+            <ul className='text-white'>
+              <li>Pizzas</li>
+              <li>Hamburguesa</li>
+              <li>Lomitos</li>
+              <li>Empanadas</li>
+              <li>Sandwich</li>
+            </ul>
+          </nav>
         </aside>
+
+        <article className='flex px-26 py-11 border border-blue-700 flex justify-center items-center'>
+          <h1 className='text-green-400'>Carta</h1>
+        </article>
+
       </section>
     </>
   )
