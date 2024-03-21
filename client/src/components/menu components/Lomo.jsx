266743b8727533a07b4aebc2f157d1a8ccabf68e
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-21 13:09:30 -0300

    menu component 3

---
+++ client/src/components/menu components/Lomo.jsx
@@ -0,0 +1,18 @@
+function Lomos () {
+  return (
+    <article>
+      <h4 className='text-xl text-white'>Nuestra Pizzas de Masa casera a la piedra</h4>
+      <p>Bife de ternera con tomate, huevo, lechuga, jamón, queso y mayonesa casera en pan de lomo. Con papas fritas</p>
+      <div>
+        <ul>
+          <li>Lomo de ternera</li>
+          <li>Lomo de pollo</li>
+          <li>Lomo XL</li>
+          <li>Lomo roquefort</li>
+        </ul>
+      </div>
+    </article>
+  )
+}
+
+export default Lomos
