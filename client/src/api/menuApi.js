2504226e8125a059f8db510cf08a47db25b40a5f
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-24 22:43:24 -0300

    Menu api

---
+++ client/src/api/menuApi.js
@@ -0,0 +1,9 @@
+const Api = [
+  {
+    id: 'Pizza',
+    title: 'Nuestra Pizzas de Masa casera a la piedra',
+    content: ['Muzzarella', 'Especial', 'Especial con anchoas']
+  }
+]
+
+export default Api
