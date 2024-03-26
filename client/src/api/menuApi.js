5da5f3746bc996cad4b96d24fc8d4a78d129921e
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-25 23:22:21 -0300

    Menu api

--- client/src/api/menuApi.js
+++ client/src/api/menuApi.js
@@ -3,13 +3,16 @@ const Api = [
     id: 'Pizza',
     title: 'Nuestra Pizzas de Masa casera a la piedra',
     subTitle: null,
-    list: ['Muzzarella', 'Especial', 'Especial con anchoas', 'Especial con huevo', 'Especial napolitana', 'Especial palmitos', 'Especial Punto fragueiro', 'Roquefort', 'Fugazza', 'Ananá', '4 quesos', 'Rúcula', 'Calabresa']
+    list: 'Muzzarella', 'Especial', 'Especial con anchoas', 'Especial con huevo', 'Especial napolitana', 'Especial palmitos', 'Especial Punto fragueiro', 'Roquefort', 'Fugazza', 'Ananá', '4 quesos', 'Rúcula', 'Calabresa']
   },
   {
     id: 'Lomitos',
     title: '',
     subTitle: 'Bife de ternera con tomate, huevo, lechuga, jamón, queso y mayonesa casera en pan de lomo. Con papas fritas',
     list: ['Lomo de ternera', 'Lomo de pollo', 'Lomo XL', 'Lomo roquefort']
+  },
+  {
+    id: 'Hamburguesas'
   }
 ]
 
