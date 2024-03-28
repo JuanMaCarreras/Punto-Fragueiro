ec472cfaaf7a12e64ba026a101dcbdf7edbfbca7
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-28 13:05:29 -0300

    menu nav api

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,37 +1,38 @@
 import { useState } from 'react'
 import Api from '../api/menuApi'
-import Pizza from "./menuComponents/Pizza"
 import MenuList from './menuComponents/MenuList'
 
 function Menu () {
 
-    const [idList, setIdList] = useState('Pizza')
+    const [idList, setIdList] = useState(null)
 
-    console.log(idList)
+
+    const handlerItemClick = (id)  => {
+    
+        setIdList(id)
+        console.log(id)
+    }
+    
   return (
     <>
       <section className='flex  justify-start bg-menuBackground h-screen py-28 px-16'>
         <aside className='border border-red-500 '>
           <nav className='px-10 py-8 '>
             <ul className='text-white py-8 [&>li]:text-3xl [&>li]:pb-5 '>
-              {/* <li>Pizzas</li>
-              <li>Hamburguesa</li>
-              <li>Lomitos</li>
-              <li>Empanadas</li>
-              <li>Sandwich</li> */}
-              {/* {
-                Api.map(index => (
-                  <li key={index.id}>
-                    <button >{index.id}</button>
+              {
+                Api.map(item => (
+                  <li key={item.id}>
+                    <button onClick={() => handlerItemClick(item.id) }>{item.id}</button>
                   </li>
                 ))
-              } */}
+              }
             </ul>
           </nav>
         </aside>
         <div className='w-full ml-24 border border-red-600'>
-          {/* <Pizza /> */}
-          <MenuList categoryData={idList}/>
+
+          <MenuList categoryId={idList}/>
+        
         </div>
       </section>
     </>
