e5c669bd8558d40c21c60fd726c1890e18651ae9
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-31 17:32:47 -0300

    Add Menu and MenuList components with dynamic rendering based on selected category

--- client/src/components/menuComponents/MenuList.jsx
+++ client/src/components/menuComponents/MenuList.jsx
@@ -1,18 +1,26 @@
-import Api from "../../api/menuApi"
+import Api from '../../api/menuApi'
 
 function MenuList ({ categoryId }) {
-
-  const categoryData = Api.find(item => item.id === categoryId);
-
+  const categoryData = Api.find(item => item.id === categoryId)
 
   return (
     <>
-    {categoryData && ( 
-      <div>
-        <h4>{categoryData.title}</h4>
-      </div>
-    )}
-  </>
+      {categoryData && (
+        <article className=' border border-green-600 my-11'>
+          <h4 className=' flex justify-center text-3xl pb-8 text-white'>{categoryData.title}</h4>
+          <p className='text-white'>{categoryData.subTitle}</p>
+          <div className=' mt-5'>
+            <ul className='ml-11 list-disc grid grid-cols-2 [&>li]:text-white'>
+              {
+              categoryData.list.map(item => (
+                <li key={item}>{item}</li>
+              ))
+              }
+            </ul>
+          </div>
+        </article>
+      )}
+    </>
   )
 }
 
