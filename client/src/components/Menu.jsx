c28a74dc47e657211a4853a18fa6118e84080672
JuanMaCarrearas <manuucarreras@gmail.com>
2024-03-31 17:32:21 -0300

    Add Menu and MenuList components with dynamic rendering based on selected category

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -3,26 +3,23 @@ import Api from '../api/menuApi'
 import MenuList from './menuComponents/MenuList'
 
 function Menu () {
+  const [idList, setIdList] = useState()
 
-    const [idList, setIdList] = useState(null)
+  const handlerItemClick = (id) => {
+    setIdList(id)
+    console.log(id)
+  }
 
-
-    const handlerItemClick = (id)  => {
-    
-        setIdList(id)
-        console.log(id)
-    }
-    
   return (
     <>
       <section className='flex  justify-start bg-menuBackground h-screen py-28 px-16'>
-        <aside className='border border-red-500 '>
+        <aside className=' '>
           <nav className='px-10 py-8 '>
-            <ul className='text-white py-8 [&>li]:text-3xl [&>li]:pb-5 '>
+            <ul className='flex-col justify-start text-white py-8  [&>li]:text-3xl [&>li]:pb-5 '>
               {
                 Api.map(item => (
                   <li key={item.id}>
-                    <button onClick={() => handlerItemClick(item.id) }>{item.id}</button>
+                    <button onClick={() => handlerItemClick(item.id)}>{item.id}</button>
                   </li>
                 ))
               }
@@ -31,8 +28,8 @@ function Menu () {
         </aside>
         <div className='w-full ml-24 border border-red-600'>
 
-          <MenuList categoryId={idList}/>
-        
+          <MenuList categoryId={idList} />
+
         </div>
       </section>
     </>
