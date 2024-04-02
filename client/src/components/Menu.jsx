76386e673256cb5a22016c90c0f50fed386a691d
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-02 20:34:15 -0300

    changing styles

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -7,19 +7,22 @@ function Menu () {
 
   const handlerItemClick = (id) => {
     setIdList(id)
-    console.log(id)
   }
 
   return (
     <>
       <section className='flex  justify-start bg-menuBackground h-screen py-28 px-16'>
         <aside className='border-l-[1px] border-r-[1px]   '>
-          <nav className='px-20 py-8 '>
+          <nav className='pl-20 pr-32 py-8 '>
             <ul className='flex-col justify-start text-white py-8  [&>li]:text-3xl [&>li]:pb-5 '>
               {
                 Api.map((item, index) => (
                   <li key={index}>
-                    <button onClick={() => handlerItemClick(item.id)}>{item.id}</button>
+                    <button
+                      className={idList === item.id ? 'text-orange translate-x-3' : 'text-white  transition delay-150 hover:translate-x-3 hover:text-orage'}
+                      onClick={() => handlerItemClick(item.id)}
+                    >{item.id}
+                    </button>
                   </li>
                 ))
               }
