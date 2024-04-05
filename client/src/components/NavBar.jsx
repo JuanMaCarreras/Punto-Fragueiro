a8075c2d125868c6017e6e17a064d6968174373b
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-05 17:56:14 -0300

    new icons

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import Logo from '../assets/logo.png'
 import { BsWhatsapp } from 'react-icons/bs'
-import { MdOutlineAccessTime } from 'react-icons/md'
+import { GiKnifeFork } from 'react-icons/gi'
+import { RiHome2Line } from 'react-icons/ri'
+import { GoLocation } from 'react-icons/go'
+import { IoMdTime } from 'react-icons/io'
 
 function NavBar () {
   return (
@@ -16,20 +19,29 @@ function NavBar () {
 
         <nav className='mr-[-50px]'>
           <ul className='flex  text-white space-x-8 '>
-            <li>Inicio</li>
-            <li>Menú</li>
-            <li>Ubicación</li>
+            <li className='flex items-center font-semibold'>
+              <RiHome2Line className=' h-5 w-5 mr-1' />
+              Inicio
+            </li>
+            <li className='flex items-center  font-semibold'>
+              <GiKnifeFork className=' h-4 w-4 mr-1' />
+              Menú
+            </li>
+            <li className='flex items-center  font-semibold'>
+              <GoLocation className=' h-5 w-5 mr-1 mb-1' />
+              Ubicación
+            </li>
           </ul>
         </nav>
 
         <nav className=''>
-          <ul className='flex   text-white space-x-6 '>
-            <li className='flex'>
-              <MdOutlineAccessTime
+          <ul className='flex  text-white space-x-6 '>
+            <li className='flex items-center  font-semibold'>
+              <IoMdTime
                 className=' h-6 w-6 mr-1'
               /> Horarios
             </li>
-            <li>
+            <li className='flex items-center'>
               <a href='https://api.whatsapp.com/send?phone=543514086715&amp;text=HOLA!%20quiero%20hacer%20un%20pedido' title='Mensaje de whatsapp' class='btn ' target='_blank' rel='noreferrer'><i class='fab fa-whatsapp' />
                 <BsWhatsapp
                   className=' h-6 w-6 hover:text-green-400 transition-colors duration-200 delay-200'
