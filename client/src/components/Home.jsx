5d8f405d43f421ed7a60949b991fcc8bcf0f0eb3
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-09 19:01:02 -0300

    id for navbar section

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,7 @@
 function Home () {
   return (
     <>
-      <main className='flex flex-col justify-center pl-11 bg-cover bg-center h-screen bg-mainImg'>
+      <main id='Inicio' className='flex flex-col justify-center pl-11 bg-cover bg-center h-screen bg-mainImg'>
         <h1 className='text-6xl mt-[-50px] '>Pizzería Punto Fragueiro</h1>
         <p className='text-white text-2xl '>Un lugar con buena onda</p>
       </main>
