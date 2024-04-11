ab73980629fb7063a854242de9b57b82e671a718
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-11 18:43:32 -0300

    modal

--- client/src/components/ServiceHoursModal.jsx
+++ client/src/components/ServiceHoursModal.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 function ServiceHoursModal () {
+  const [modalIsOpen, setModalIsOpen] = useState()
+
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(true)
+  }
+
   return (
     <>
+      <button onClick={openModal}>Horarios</button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel='Ejemplo de Modal'
+      >
+        <h2>Ejemplo de Modal</h2>
+        <p>Este es un ejemplo de modal en React.</p>
+        <button onClick={closeModal}>Cerrar Modal</button>
+
+      </Modal>
 
     </>
   )
