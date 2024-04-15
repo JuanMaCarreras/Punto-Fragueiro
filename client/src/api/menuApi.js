be9dd96f157f6c8b0c1fe1c256354a3205872dec
JuanMaCarrearas <manuucarreras@gmail.com>
2024-04-15 18:37:10 -0300

    Restructured API and added new items

--- client/src/api/menuApi.js
+++ client/src/api/menuApi.js
@@ -3,31 +3,44 @@ const Api = [
     id: 'Pizza',
     title: 'Pizzas',
     subTitle: 'Nuestra Pizzas de Masa casera a la piedra',
-    list: ['Muzzarella', 'Especial', 'Especial con anchoas', 'Especial con huevo', 'Especial napolitana', 'Especial palmitos', 'Especial Punto fragueiro', 'Roquefort', 'Fugazza', 'Ananá', '4 quesos', 'Rúcula', 'Calabresa']
+    items: [
+      {
+        name: 'MUZZARELLA',
+        ingredients: 'Salsa, muzzarella, orégano y aceitunas.'
+      },
+      {
+        name: 'PIZZA ESPECIAL',
+        ingredients: 'Salsa, muzzarella, jamón, morrón y aceitunas.'
+      },
+      {
+        name: 'ESPECIAL CON ANCHOAS',
+        ingredients: 'Salsa, muzzarella, jamón, anchoas, orégano y aceitunas.'
+      }
+    ]
   },
   {
     id: 'Lomitos',
     title: 'Lomitos con papas fritas',
     subTitle: 'Bife de ternera con tomate, huevo, lechuga, jamón, queso y mayonesa casera en pan de lomo. Con papas fritas',
-    list: ['Lomo de ternera', 'Lomo de pollo', 'Lomo XL', 'Lomo roquefort']
+    items: ['Lomo de ternera', 'Lomo de pollo', 'Lomo XL', 'Lomo roquefort']
   },
   {
     id: 'Hamburguesas',
     title: 'Hamburguesa con papas fritas',
     subTitle: 'Medallón de carne con tomate, lechuga, jamón, queso chedar y mayonesa casera en pan con sésamo. Con papas fritas',
-    list: ['Con 1, 2 ó 3 medallones de carne!!']
+    items: ['Con 1, 2 ó 3 medallones de carne!!']
   },
   {
     id: 'Empanadas',
     title: 'Empanadas al horno ',
     subTitle: null,
-    list: ['Arabes', 'Criollas saladas', 'Criollas dulces', 'Jamón y queso']
+    items: ['Arabes', 'Criollas saladas', 'Criollas dulces', 'Jamón y queso']
   },
   {
     id: 'Sandwiches',
     title: 'Sandwich de milanesa ',
     subTitle: ' Milanesa con tomate, lechuga, huevo, jamón, queso y mayonesa casera en pan especial para Sandwich. Con papas fritas',
-    list: ''
+    items: 'Un Alto Sandwich'
   }
 ]
 
